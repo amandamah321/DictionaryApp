41063f5e977b1d78f527cb311f4b4473cc6d1d61
amandamah321 <amanda_mah321@hotmail.com>
2024-01-29 16:34:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+
+const btnSearch = document.getElementById("btn__search")
+const inputWord = document.getElementById("input__word")
+const resultEl = document.querySelector(".result")
+
+
+btnSearch.addEventListener("click", findWord)
+
+async function findWord() {
+    const word = inputWord.value;
+    const data = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`).then((response) => response.json())
+
+    resultEl.innerHTML = 
+    `
+    <div>
+        <div class="word">
+            <h2>${word} - ${data[0].meanings[0].partOfSpeech}</h2>
+            <div class="pronunciation">
+                <p>Pronunciation: ${data[0].phonetic}</p>
+                <audio controls>
+                    <source src="${data[0].phonetics[0].audio}">
+                </audio>
+            </div>
+            <p id="meaning">${data[0].meanings[0].definitions[0].definition}.</p>
+        </div>
+    </div>
+    `
+
+    console.log(data)
+
+    deleteWord()
+}
+
+function deleteWord() {
+    inputWord.value = ""
+}
